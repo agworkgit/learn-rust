0a7fb561f88a3464eb716a8221bb56170649a62f
Alex Grigore <146381585+agworkgit@users.noreply.github.com>
2025-12-04 12:14:30 +0000

    compound data types

--- 02_primitive_data_types/main.rs
+++ 02_primitive_data_types/main.rs
@@ -8,35 +8,39 @@ fn main() {
     // Generally, the smaller the number the smaller the amount of numbers and range.
 
     let x: i32 = -42;
-    let y: u64 = 100;
+    let y: u64 = 100; // only positive values
+    println!("Signed Integer: {}", x);
+    println!("Unsigned Integer: {}", y);
+
     // Differences --- i32 (32bits), i64 (64bits)
     // Range : i32 can vary from -2^31 to +2^31 while i64 can vary from -2^63 to +2^63
     // Examples:
     // --- i32 - 2477483647
     // --- i64 - 9223372036854775807
 
-    println!("Signed Integer: {}", x);
-    println!("Unsigned Integer: {}", y);
+    let e: i32 = 2147483647;
+    let i: i64 = 922337203684775807;
+    println!("Max value of i32: {}", e);
+    println!("Max value of i64: {}", i);
+
     // Always add semicolons at the end of your statements otherwise rustc will complain
 
-    let e: i32 = 2147483647;
-    let i: i64 = 9223372036854775807;
-
-    println!("Maximum value of i32: {}", e);
-    println!("Maximum value of i64: {}", i);
     // --- u32 and u64 have a larger range than i32 and i64 but not by much, in any case the compiler will make suggestions
 
     // Floats [Floating Point Types]
     // --- f32, f64
+
     let pi: f64 = 3.14;
-    println!("Value of pi: {}", pi);
+    println!("Value of PI: {}", pi);
 
     // Boolean
     // --- true, false
+
     let is_snowing: bool = true;
     println!("Is it snowing?: {}", is_snowing);
 
-    // Character Type - char
+    // Character Type - char (single unicode value)
+
     let letter: char = 'a';
-    println!("First letter of the alphabet: {}", letter);
+    println!("The first alphabet letter is: {}", letter);
 }
--- 03_compound_data_types/main.rs
+++ 03_compound_data_types/main.rs
@@ -2,12 +2,15 @@
 // --- arrays, tuples, slices, and strings (slice string)
 
 fn main() {
-    // Arrays - Fixed size collection of elements of homogeneous type
+    // Arrays - Fixed size collection of elements of homogeneous type (same type)
     // --- We then need to specify the type and size in square brackets
+
     let numbers: [i32; 5] = [1, 2, 3, 4, 5];
     println!("Number Array: {:?}", numbers);
+
     // let mix = [1, 2, "apple", true];
     // println!("Mix Array: {:?}", mix);
+
     let fruits: [&str; 3] = ["Apple", "Banana", "Orange"];
     println!("Fruits Array: {:?}", fruits);
     println!("Fruits Array 1st element: {:?}", fruits[0]);
@@ -15,6 +18,7 @@ fn main() {
     println!("Fruits Array 3rd element: {:?}", fruits[2]);
 
     // Tuples - Tuples contain heterogeneous collections of elements of fixed size
+
     let human: (String, i32, bool) = ("Alice".to_string(), 30, false); // .to_string() converts from string slice to string data type
     println!("Human Tuple: {:?}", human);
     let my_mix_tuple = ("Kratos", 23, true, [1, 2, 3, 4, 5]);
@@ -23,6 +27,7 @@ fn main() {
     // Slices - Dynamically sized view into a contigeous sequence of elements
     // --- "contigeous" = data or memory that is stored in an unbroken, sequential block
     // [1, 2, 3, 4, 5] - "contigeous = uninterrupted, meaning all elements are adjacent to one another"
+
     let number_slices: &[i32] = &[1, 2, 3, 4, 5];
     println!("Number Slice: {:?}", number_slices);
 
@@ -42,13 +47,15 @@ fn main() {
     // * Look into heap vs stack memory
 
     // String - stored in heap memory
+
     let mut stone_cold: String = String::from("Hell, "); // 'mut' converts to mutable data type
     stone_cold.push_str("Yeah!");
     println!("Stone Cold Says: {}", stone_cold);
 
-    // &str (String Slice) - stored in stack memory, a reference to a portion of a string that's stored somewhere in your code, 'immutable'
+    // String Slice (&str) - stored in stack memory, a reference to a portion of a string that's stored somewhere in your code, 'immutable'
     // --- Used to reference string literals, or substrings, or string objects without needing to copy or own the data
     // --- Stack is quicker, Heap is slower, Stack data types are 'immutable', Heap data types are 'dynamic, or mutable'
+
     let string: String = String::from("Hello, World!");
     // --- '&string' refers to the value of the variable above, it's a copy of it
     let slice: &str = &string[0..5]; // [0..5] get only portions of a string
@@ -59,5 +66,5 @@ fn main() {
     fn print() {
         println!("SLICE: {}", slice);
         // ------------------ ^^^^^ not found in this scope
-} 
+}
 */
